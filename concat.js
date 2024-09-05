abc4618015e4f9952e23271ceb47eced6bd2869e
appoclayptor <shahreza1414@gmail.com>
2024-09-05 16:12:13 +0600

    conacatenatin problem solved

---
+++ concat.js
@@ -0,0 +1,18 @@
+/**
+ * ### Task 3
+
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+**Input:**
+`var numbers = ['Tom', 'Tim', 'Tin', 'Tik']`
+
+**Output:**
+
+`'TomTimTinTik'`
+
+---
+ */
+
+const name = ['Tom', 'Tim', 'Tin', 'Tik'];
+
+console.log(name.join(''));
