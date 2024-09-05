f6c85baea7a11acc66989b646ffad93daeee0205
appoclayptor <shahreza1414@gmail.com>
2024-09-05 16:20:55 +0600

    Sentence reversal solved

---
+++ sentence_reversal.js
@@ -0,0 +1,18 @@
+/**
+ * ### Task 4 (Hard)
+
+Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+**Input:**
+`const statement = 'I am a hard working person'`
+
+**Output:**
+
+`'person working hard a am I'`
+
+---
+ */
+
+const sentence = 'I am the father of Rayan';
+
+console.log(sentence.split(' ').reverse().join(' '));
